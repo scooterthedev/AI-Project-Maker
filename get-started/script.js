60bae45efba335a44d93b1f18f10ceab1892b4e3
Scooter <talkufy@outlook.com>
2024-06-30 21:05:16 -0400

    Added some more fixes to registration and some npm packages were added

--- get-started/script.js
+++ get-started/script.js
@@ -1,16 +1,21 @@
-import { createClient } from '@supabase/supabase-js'
+
 
 const supabaseUrl = 'https://lllitinjwarzhqnfxkur.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsbGl0aW5qd2FyemhxbmZ4a3VyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk3Nzk0MjUsImV4cCI6MjAzNTM1NTQyNX0.jXiEA7f-cpOBD_1N-UaVrtCDGE7ovglAzHvT_L1LAug';
 const supabase = createClient(supabaseUrl, supabaseKey);
 // Register form submission
-const registerForm = document.getElementById('login-container');
+const registerForm = document.getElementById('login-button');
 
 registerForm.addEventListener('submit', async (event) => {
     event.preventDefault();
+    console.log('Form submitted');  
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+
+    console.log('Email:', email);  
+    console.log('Password:', password); 
+
     async function signUpNewUser(){
     try {
         const { user, error } = await supabase.auth.signUp({
