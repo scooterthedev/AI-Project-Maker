9b0adaa19a3116df3d20db98d174b0a2f05eef60
Scooter <info@talkufy.com>
2024-08-08 21:12:30 -0400

    Deleted Unnecessary Stuff

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const grid = document.querySelector('.grid');
-const numBlocks = 130; // Number of blocks to cover the page, adjust as needed
+const numBlocks = 130; // Number of blocks to cover the page
 
-// Create blocks dynamically
 for (let i = 0; i < numBlocks; i++) {
     const block = document.createElement('div');
     block.classList.add('block');
